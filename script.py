d6cd449f039d0dc8eb75851dec381b7b17fd101e
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2025-02-20 18:04:43 +0200

    Implement face detection using RetinaFace

--- script.py
+++ script.py
@@ -3,3 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from retinaface import RetinaFace
 from deepface import DeepFace
+
+
+input_image_path = "input.jpg"
+reference_image_path = "reference.jpg"
+
+detected_faces = RetinaFace.detect_faces(input_image_path)
+print(detected_faces)
